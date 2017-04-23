fdec651b449497c605ed4a0518b5b0486a48b4b7
dcast78 <dcast78@users.noreply.github.com>
2017-04-23 18:57:35 +0200

    Update I2cController.ino

--- portable/sketchbook/I2cController/I2cController.ino
+++ portable/sketchbook/I2cController/I2cController.ino
@@ -116,11 +116,7 @@ void setup() {
 	contr.setupEncoder(EN_A,EN_B,EN_C); // Encoder setup 
 //	contr.setIntBtn(BTN_1);                 // Single button setup
 	//contr.setupInterruptPin(BTN_1, FALLING); //Use this if you want receive continuos interrupt on button pressed. Useful ??
-//	contr.setIntBtn(BTN_2);                 // Single button setup
-//	contr.setIntBtn(BTN_3);                 // Single button setup
-//	contr.setIntBtn(BTN_4);                 // Single button setup
-//	contr.setIntBtn(BTN_5);                 // Single button setup
-        uint8_t btnCross[BTN_NUM] = { BTN_1, BTN_2, BTN_3, BTN_4, BTN_5};
+        uint8_t btnCross[BTN_NUM] = { BTN_1, BTN_2, BTN_3, BTN_4, BTN_5}; // Five button on cross disposition, setup function
         contr.setIntCross(btnCross, BTN_NUM);
 	attachInterrupt(arduinoInterrupt, intCallBack, FALLING);
 
